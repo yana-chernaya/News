c462324e10038c0f0b6377707a41ac13ee97a84c
Yana Chernaya <_yi5470365@gmail.com>
2026-05-19 15:00:54 +0300

    Add NewsMapper and NewsRepositoryImpl

--- app/src/main/java/com/example/news/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/example/news/data/repository/NewsRepositoryImpl.kt
@@ -1,38 +1,78 @@
 package com.example.news.data.repository
 
+import android.util.Log
+import com.example.news.data.local.ArticleDbModel
+import com.example.news.data.local.NewsDao
+import com.example.news.data.local.SubscriptionDbModel
+import com.example.news.data.mapper.toDbModels
+import com.example.news.data.mapper.toEntities
+import com.example.news.data.mapper.toQueryParam
+import com.example.news.data.remote.NewsApiService
 import com.example.news.domain.entity.Article
 import com.example.news.domain.entity.Language
 import com.example.news.domain.repository.NewsRepository
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class NewsRepositoryImpl @Inject constructor(): NewsRepository {
-    
+class NewsRepositoryImpl @Inject constructor(
+    private val newsDao: NewsDao,
+    private val newsApiService: NewsApiService,
+) : NewsRepository {
+
     override fun getAllSubscriptions(): Flow<List<String>> {
-        TODO("Not yet implemented")
+        return newsDao.getAllSubscriptions().map { subscriptions ->
+            subscriptions.map { it.topic }
+        }
     }
 
     override suspend fun addSubscription(topic: String) {
-        TODO("Not yet implemented")
+        newsDao.addSubscription(SubscriptionDbModel(topic))
     }
 
     override suspend fun updateArticlesForTopic(topic: String, language: Language) {
-        TODO("Not yet implemented")
+        val articles = loadArticles(topic, language)
+        newsDao.addArticles(articles)
+    }
+
+    private suspend fun loadArticles(topic: String, language: Language): List<ArticleDbModel> {
+        return try {
+            newsApiService.loadArticles(topic, language.toQueryParam()).toDbModels(topic)
+        } catch (e: Exception) {
+            if (e is CancellationException) {
+                throw e
+            }
+            Log.e("NewsRepository", e.stackTraceToString())
+            listOf()
+        }
     }
 
     override suspend fun removeSubscription(topic: String) {
-        TODO("Not yet implemented")
+        newsDao.deleteSubscription(SubscriptionDbModel(topic))
     }
 
     override suspend fun updateArticlesForAllSubscriptions(language: Language) {
-        TODO("Not yet implemented")
+        val subscriptions = newsDao.getAllSubscriptions().first()
+        coroutineScope {
+            subscriptions.forEach {
+                launch {
+                    updateArticlesForTopic(it.topic, language)
+                }
+            }
+        }
     }
 
     override fun getArticlesByTopics(topics: List<String>): Flow<List<Article>> {
-        TODO("Not yet implemented")
+        return newsDao.getAllArticlesByTopics(topics).map {
+            it.toEntities()
+        }
     }
 
     override suspend fun clearAllArticles(topics: List<String>) {
-        TODO("Not yet implemented")
+        newsDao.deleteArticlesByTopics(topics)
     }
 }
