9582776b16bf382e9cd689ab89e8aefcdfa4eb7e
Yana Chernaya <_yi5470365@gmail.com>
2026-05-21 12:05:12 +0300

    Create SubscriptionsViewModel and connect it to SubscriptionsScreen

--- app/src/main/java/com/example/news/presentation/MainActivity.kt
+++ app/src/main/java/com/example/news/presentation/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.news.presentation.screen.subscriptions.SubscriptionsScreen
 import com.example.news.presentation.ui.theme.NewsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,29 +15,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SubscriptionsScreen(
+                    onNavigateToSettings = {}
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsTheme {
-        Greeting("Android")
     }
 }
