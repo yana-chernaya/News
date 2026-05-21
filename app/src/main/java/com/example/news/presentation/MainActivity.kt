5b58652a072e33d042b12fcbb6aea820728bef0d
Yana Chernaya <_yi5470365@gmail.com>
2026-05-21 12:57:04 +0300

    Setup app navigation

--- app/src/main/java/com/example/news/presentation/MainActivity.kt
+++ app/src/main/java/com/example/news/presentation/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.news.presentation.screen.settings.SettingsScreen
+import com.example.news.presentation.navigation.NavGraph
 import com.example.news.presentation.ui.theme.NewsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,9 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsTheme {
-                SettingsScreen(
-                    onBackClick = {}
-                )
+                NavGraph()
             }
         }
     }
---
+++ app/src/main/java/com/example/news/presentation/navigation/NavGraph.kt
@@ -0,0 +1,39 @@
+package com.example.news.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.news.presentation.screen.settings.SettingsScreen
+import com.example.news.presentation.screen.subscriptions.SubscriptionsScreen
+
+@Composable
+fun NavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Subscriptions.route
+    ) {
+        composable(Screen.Subscriptions.route) {
+            SubscriptionsScreen(
+                onNavigateToSettings = {
+                    navController.navigate(Screen.Settings.route)
+                }
+            )
+        }
+
+        composable(Screen.Settings.route) {
+            SettingsScreen(
+                onBackClick = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
+
+sealed class Screen(val route: String) {
+    data object Subscriptions : Screen("subscriptions")
+    data object Settings : Screen("settings")
+}
